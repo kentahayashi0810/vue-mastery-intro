fea8bca52b246e6af4d7f4d6e52f72571a4f2d0d
kentahayashi <kenta0810hayashi@gmail.com>
2023-07-21 22:22:42 +0200

    commit

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-<<<<<<< HEAD
-      product: "shoes",
-      desc: "These shoes smell interesting!!!",
-=======
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       alt: "Green socks with M logo on them",
       link: "https://www.youtube.com/",
->>>>>>> L3-start
     };
   },
 });
