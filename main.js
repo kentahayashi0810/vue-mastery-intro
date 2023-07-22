0def8ee5d747f74f20f7e89b7ad6853daafe3757
kentahayashi <kenta0810hayashi@gmail.com>
2023-07-22 18:29:30 +0200

    L6-start

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 const app = Vue.createApp({
   data() {
     return {
-<<<<<<< HEAD
-=======
       cart: 0,
->>>>>>> L6-start
       product: "Socks",
       image: "./assets/images/socks_blue.jpg",
       inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-<<<<<<< HEAD
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
-      ],
-      sizes: ["Small", "Medium", "Large"],
-    };
-  },
-=======
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
         { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
@@ -36,5 +25,4 @@ const app = Vue.createApp({
       this.image = variantImage;
     },
   },
->>>>>>> L6-start
 });
